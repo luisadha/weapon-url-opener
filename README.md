d732140c51d1f80aa2a1875752a977b1d7aef039
Luis Adha <40796508+luisadha@users.noreply.github.com>
2024-03-16 22:11:55 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# weapon-url-opener
+#Decriptions
+
+Beralih script downloader dengan mudah dan cepat. Sekarang fitur termux-url-opener lebih sering anda gunakan berkat skrip pengelola ini.
+
+# Usage
+Cara mengunduh skrip downloader?
+1. Cari skrip downloader di github atau sumber manapun yang menyertakan ini untuk penggunaan termux.
+NOTE : minimal download 2 skrip downloader berbeda sebelum menggunakan pengelola ini (jika tidak anda mungkin tidak memerlukan ini).
+2. Unduh dan hanya letakan skrip downloader di directory ~/bin
+3. Sudah hanya itu!. Jalankan skrip jika anda ingin berganti skrip downloader yang berbeda.
+
+# Compability
+- ztmexluis v1.2
+- Termux:Widget
+
+# Author
+@luisadha
+
---
+++ weapon-url-opener.sh
@@ -0,0 +1,46 @@
+while true; do
+cat << "EOF"
+ _       __                                              __                                      
+| |     / /__  ____ _____  ____  ____        __  _______/ /     ____  ____  ___  ____  ___  _____
+| | /| / / _ \/ __ `/ __ \/ __ \/ __ \______/ / / / ___/ /_____/ __ \/ __ \/ _ \/ __ \/ _ \/ ___/
+| |/ |/ /  __/ /_/ / /_/ / /_/ / / / /_____/ /_/ / /  / /_____/ /_/ / /_/ /  __/ / / /  __/ /    
+|__/|__/\___/\__,_/ .___/\____/_/ /_/      \__,_/_/  /_/      \____/ .___/\___/_/ /_/\___/_/     
+                 /_/                                              /_/                           
+EOF
+
+echo -e $C " ╔════════════════════════════════════════════════╗"
+echo -e $G " )⇒ Project Name  :weapon-url-opener             ⇐("
+echo -e $G " )⇒ Author        :luisadha                      ⇐("       
+echo -e $G " )⇒ Github        :github.com/luisadha           ⇐("
+echo -e $G " )⇒ Telegram      :@adharudin14                  ⇐("        
+echo -e $C " ╚════════════════════════════════════════════════╝"
+echo -e " Tips: Use CTRL + C to safe exit!\n"
+test -f ~/bin/termux-url-opener
+if [ $? -eq 1 ]; then
+  tidakada="(service not used)"
+  echo -n " IN USE: $tidakada"; echo
+else
+  echo -n " IN USE: "; basename "$(cat .weapon-url-opener.status 2>/dev/null | awk '{print $2}')"; echo
+fi
+echo 
+file_list=($(ls -Art ~/bin/*.sh))
+file_list=( $(for file in "${file_list[@]}"; do basename "$file"; done) )
+  select weapon in "${file_list[@]}" "EXIT"; do
+    {
+      diff -sq ~/bin/"$weapon" ~/bin/termux-url-opener > .weapon-url-opener.status 2>/dev/null
+    }
+    [[ -n "$weapon" ]] || { break; }
+ if [[ "$weapon" == "EXIT" ]]; then
+      echo "Exiting the program"
+      rm -f .weapon-url-opener.status
+      break 2
+    else
+      echo "You selected `basename $weapon` as primary weapon"
+      sleep 1
+      cp -f ~/bin/"$(basename $weapon)" ~/bin/termux-url-opener   
+      break
+    fi
+  done
+done
+
+
