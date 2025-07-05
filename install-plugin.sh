c49085865ca4690d8e94f0742dfdea6e6b396e13
Luis Adha <40796508+luisadha@users.noreply.github.com>
2025-07-06 01:56:15 +0700

    Update install-plugin.sh

--- install-plugin.sh
+++ install-plugin.sh
@@ -1,13 +1,16 @@
-opt=(--add-repo "$@")
+opt='--add-repo "$@"'
 
 if command -v ztme-toolbox >/dev/null 2>&1 || command -v weapon-url-opener >/dev/null 2>&1; then
-  cat > ~/bin/ztme-toolbox.sh <<EOF
+ echo -e "Performing to install.. plugin (ztme-toolbox)" 
+cat > ~/bin/ztme-toolbox.sh <<EOF
 #!/data/data/com.termux/files/usr/bin/bash
-ztme-toolbox "${opt[@]}" 2>&1 || weapon-url-opener "${opt[@]}" 2>&1
+ztme-toolbox ${opt}" 2>&1 || weapon-url-opener ${opt}" 2>&1
 EOF
   chmod +x ~/bin/ztme-toolbox.sh
 else
-  #echo "Please install salah satu dari weapon-url-opener dulu"
+  echo "Installing requirement.."
   wget https://luisadha.github.io/weapon-url-opener -O ~/.local/bin/weapon-url-opener
   chmod +x ~/.local/bin/weapon-url-opener
+  echo "..done" | pv -qL10
+  echo -e "Please run again this script"
 fi
