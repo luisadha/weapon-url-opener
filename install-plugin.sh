4342893eb575c8448afb4416b5e4d9d5bae75fb5
Luis Adha <40796508+luisadha@users.noreply.github.com>
2025-07-06 01:59:18 +0700

    Update install-plugin.sh

--- install-plugin.sh
+++ install-plugin.sh
@@ -4,10 +4,11 @@ if command -v ztme-toolbox >/dev/null 2>&1 || command -v weapon-url-opener >/dev
  echo -e "Performing to install.. plugin (ztme-toolbox)" 
 cat > ~/bin/ztme-toolbox.sh <<EOF
 #!/data/data/com.termux/files/usr/bin/bash
-ztme-toolbox ${opt}" 2>&1 || weapon-url-opener ${opt}" 2>&1
+ztme-toolbox ${opt} 2>&1 || weapon-url-opener ${opt} 2>&1
 EOF
   chmod +x ~/bin/ztme-toolbox.sh
-else
+  echo "..done" | pv -qL 10
+ else
   echo "Installing requirement.."
   wget https://luisadha.github.io/weapon-url-opener -O ~/.local/bin/weapon-url-opener
   chmod +x ~/.local/bin/weapon-url-opener
